a1d478359f5abd6fa03e96e4e67224305ca3181b
QHwan <poter1026@gmail.com>
2020-04-30 15:56:21 +0900

    file management

---
+++ data/data_convert.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+from collections import Counter
+
+from rdkit import Chem
+from rdkit.Chem.rdmolops import GetAdjacencyMatrix
+
+
+raw_dataset_filename = './dataset/freesolv.csv'
+raw_dataset = pd.read_csv(raw_dataset_filename)
+
+smiles = raw_dataset['smiles']
+solv_energies = raw_dataset['expt']
+
+atom_types = []
+num_hs = []
+num_valences = []
+aromaticities = []
+for i, smile in enumerate(smiles):
+    mol = Chem.MolFromSmiles(smile)
+    atoms = mol.GetAtoms()
+
+    atom_types += [atom.GetSymbol() for atom in atoms]
+    num_hs += [atom.GetTotalNumHs() for atom in atoms]
+    num_valences += [atom.GetImplicitValence() for atom in atoms]
+    aromaticities += [int(atom.GetIsAromatic()) for atom in atoms]
+
+atom_type_dict = {}
+num_h_dict = {}
+num_valence_dict = {}
+aromaticity_dict = {}
+for i, key in enumerate(Counter(atom_types).keys()):
+    atom_type_dict[key] = i
+for i, key in enumerate(sorted(Counter(num_hs).keys())):
+    num_h_dict[key] = i
+for i, key in enumerate(sorted(Counter(num_valences).keys())):
+    num_valence_dict[key] = i
+for i, key in enumerate(sorted(Counter(aromaticities).keys())):
+    aromaticity_dict[key] = i
+
+
+Xs = []; As = []; Ys = []
+for i, smile in enumerate(smiles):
+    mol = Chem.MolFromSmiles(smile)
+    atoms = mol.GetAtoms()
+
+    atom_types = [atom.GetSymbol() for atom in atoms]
+    num_hs = [atom.GetTotalNumHs() for atom in atoms]
+    num_valences = [atom.GetImplicitValence() for atom in atoms]
+    aromaticities = [int(atom.GetIsAromatic()) for atom in atoms]
+
+    X = []
+    for j, (atom_type, num_h, num_valence, aromaticity) in enumerate(zip(atom_types,
+                                                                         num_hs,
+                                                                         num_valences,
+                                                                         aromaticities)):
+        x_atom_type = np.zeros(len(atom_type_dict))
+        x_num_h = np.zeros(len(num_h_dict))
+        x_num_valence = np.zeros(len(num_valence_dict))
+        x_aromaticity = np.zeros(len(aromaticity_dict))
+
+        x_atom_type[atom_type_dict[atom_type]] += 1
+        x_num_h[num_h_dict[num_h]] += 1
+        x_num_valence[num_valence_dict[num_valence]] += 1
+        x_aromaticity[aromaticity_dict[aromaticity]] += 1
+
+        x = np.concatenate((x_atom_type,
+                            x_num_h,
+                            x_num_valence,
+                            x_aromaticity))
+        X.append(x)
+
+    A = GetAdjacencyMatrix(mol) + np.eye(len(atoms))
+    Y = solv_energies[i]
+
+    Xs.append(np.array(X))
+    As.append(np.array(A))
+    Ys.append(np.array(Y))
+
+
+np.savez('dataset/freesolv.npz', Xs=Xs, As=As, Ys=Ys)
---
+++ data/data_prepare.py
@@ -0,0 +1,56 @@
+import random
+import numpy as np
+import torch
+
+def data_prepare(filename,
+                train_ratio, val_ratio, test_ratio):
+    f = np.load(filename, allow_pickle=True)
+
+    Xs = f['Xs']
+    As = f['As']
+    Ys = f['Ys']
+
+    num_data = len(Ys)
+    num_train = int(num_data*train_ratio)
+    num_val = int(num_data*val_ratio)
+    num_test = num_data - num_train - num_val
+
+    idx = list(range(num_data))
+    random.shuffle(idx)
+    idx_train = idx[:num_train]
+    idx_val = idx[num_train:num_train+num_val]
+    idx_test = idx[num_train+num_val:]
+
+    Xs_train = Xs[idx_train]
+    Xs_val = Xs[idx_val]
+    Xs_test = Xs[idx_test]
+
+    As_train = As[idx_train]
+    As_val = As[idx_val]
+    As_test = As[idx_test]
+
+    Ys_train = Ys[idx_train]
+    Ys_val = Ys[idx_val]
+    Ys_test = Ys[idx_test]
+
+    # Y normalization
+    if abs(max(Ys)) > abs(min(Ys)):
+        norm_value = max(Ys)
+    else:
+        norm_value = min(Ys)
+    Ys_train /= norm_value
+    Ys_val /= norm_value
+    Ys_test /= norm_value
+
+    print(Ys_train)
+
+    return((Xs_train, Xs_val, Xs_test),
+           (As_train, As_val, As_test),
+           (Ys_train, Ys_val, Ys_test),
+           norm_value)
+
+if __name__ == "__main__":
+    data_prepare(filename='dataset/freesolv.npz',
+                train_ratio=.8,
+                val_ratio=.1,
+                test_ratio=.1)
